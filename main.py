87cb737fdc3506e8133d0fc81187e58c1ef3be2d
Roman Shupak <romanshupak808@gmail.com>
2024-06-12 20:12:09 +0300

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from rest_framework.utils import json
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serialized_car = CarSerializer(car)
+    return json.dumps(serialized_car)
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_data: bytes) -> Car:
+    deserialized_data = json.loads(json_data)
+    car_serializer = CarSerializer(data=deserialized_data)
+    if car_serializer.is_valid():
+        return car_serializer.save()
+    else:
+        raise ValueError("Invalid data for deserialization")
+
