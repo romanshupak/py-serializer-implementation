020e13bad9b934db09fb0b51540c993b4109baea
Roman Shupak <romanshupak808@gmail.com>
2024-06-13 20:17:41 +0300

    fix serialize and deserialize functions

--- car/serializers.py
+++ car/serializers.py
@@ -6,13 +6,15 @@ from car.models import Car
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    manufacturer = serializers.CharField(max_length=255, required=True)
-    model = serializers.CharField(max_length=255, required=True)
+    manufacturer = serializers.CharField(max_length=64, required=True)
+    model = serializers.CharField(max_length=64, required=True)
     horse_powers = serializers.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(1914)]
+        validators=[MaxValueValidator(1914), MinValueValidator(1)]
     )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(null=True, blank=True)
+    problem_description = serializers.CharField(
+        allow_blank=True, required=False, max_length=255
+    )
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from rest_framework.utils import json
-
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from car.models import Car
 from car.serializers import CarSerializer
+import io
 
 
 def serialize_car_object(car: Car) -> bytes:
     serialized_car = CarSerializer(car)
-    return json.dumps(serialized_car)
+    return JSONRenderer().render(serialized_car.data)
 
 
 def deserialize_car_object(json_data: bytes) -> Car:
-    deserialized_data = json.loads(json_data)
-    car_serializer = CarSerializer(data=deserialized_data)
+    deserialized_data = io.BytesIO(json_data)
+    data = JSONParser().parse(deserialized_data)
+    car_serializer = CarSerializer(data=data)
     if car_serializer.is_valid():
-        return car_serializer.save()
-    else:
-        raise ValueError("Invalid data for deserialization")
+        car = Car(**car_serializer.validated_data)
+        car.save()
+        return car
